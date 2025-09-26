1e309a1174b517d613b3ac91c4fa5f6bc6c1a471
Chapman Shoop <chapman.shoop@gmail.com>
2025-09-26 15:04:36 -0500

    Left mouse camera flash, WASD to move.

---
+++ camera.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("flash"):
+		$Flash.visible = true
+		$Timer.start()
+
+func _on_timer_timeout() -> void:
+	$Flash.visible = false
