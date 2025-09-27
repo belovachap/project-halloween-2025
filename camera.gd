fb98b50476863a4cb185ca02c93888888d8cf555
Chapman Shoop <chapman.shoop@gmail.com>
2025-09-26 22:27:23 -0500

    Camera flash noise sampled from: https://freesound.org/people/thecheeseman/sounds/51360/

--- camera.gd
+++ camera.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("flash"):
+		$FlashSound.play()
 		$Flash.visible = true
 		$Timer.start()
 
