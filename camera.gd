ffbe63da2453f30e5faa0f99fe3e678cd0e16bf7
Chapman Shoop <chapman.shoop@gmail.com>
2025-09-28 07:18:41 -0500

    Use JOLT physics. Working on how to detect if camera flash hits a monster...

--- camera.gd
+++ camera.gd
@@ -1,16 +1,21 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("flash"):
 		$FlashSound.play()
 		$Flash.visible = true
 		$Timer.start()
+
+	if $Flash.visible:
+		if $ShapeCast3D.is_colliding():
+			for i in range($ShapeCast3D.get_collision_count()):
+				var collider = $ShapeCast3D.get_collider(i)
+				# Process the collision information
+				#print("Collision detected:")
+				#print("  Collider: ", collider.name)
+				if collider.name == "Monster":
+					collider.hit_by_flash()
 
 func _on_timer_timeout() -> void:
 	$Flash.visible = false
---
+++ the_maze.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+func _ready() -> void:
+	# Turn off the lights!
+	$Areas/TheCells/StartingCell2/OmniLight3D.visible = false
+	$Areas/TheCells/StartingCell3/OmniLight3D.visible = false
+	$Areas/TheCells/StartingCell4/OmniLight3D.visible = false
+	$Areas/TheCells/StartingCell5/OmniLight3D.visible = false
+	$Areas/TheCells/StartingCell6/OmniLight3D.visible = false
